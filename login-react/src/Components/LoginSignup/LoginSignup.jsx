92940053cd8a505f3dcf6ff3bf6dc77130def757
ashkam58 <ashkam58@gmail.com>
2024-01-26 23:56:40 +0530

    added new components

--- login-react/src/Components/LoginSignup/LoginSignup.jsx
+++ login-react/src/Components/LoginSignup/LoginSignup.jsx
@@ -3,6 +3,7 @@ import './LoginSignup.css'
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
+import border from '../Assets/pngwing.com.png'
 
 export const LoginSignup = () => {
   return (
